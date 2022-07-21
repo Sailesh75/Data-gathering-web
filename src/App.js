d7dd496d714e6f654b14904d1c03fd4019dbe081
SaileshKafle <saileshkafle47@gmail.com>
2022-07-21 11:02:23 +0545

    onClick event added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import LandingPage from "./component/LandingPage";
+// import Recording from "./component/Recording";
 
 function App() {
   return (
     <div className="App">
      <LandingPage/>
-     
-
-      
+     {/* <Recording/> */}
     </div>
   );
 }
--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './landingpage.scss'
 import { BsMic} from "react-icons/bs";
 import ReactPlayer from 'react-player';
 
+
 const LandingPage = () => {
+    const[isActive,setIsActive]=useState(false);
+    const handleClick=()=>{
+        setIsActive(current=>!current)
+    };
+    const stopClick=()=>{
+        setIsActive(false)
+
+    };
+    
   return (
         <div className="top">
             <div className="left">
@@ -16,11 +26,17 @@ const LandingPage = () => {
                     <p>Read and record the text below.</p>
                     <div className="card">
                         <p className='textLabel'>हामीले  हामीलाई एउटा मद्दत </p>
+                        
                         <button  
-                        // style={{onClick? }}
-                        onClick={()=>{console.log('clicked!!')}} 
+                        style={{
+                            border: isActive? 'solid 1px #00b33c':'',
+                            backgroundColor:isActive? '#00b33c':'',  
+                        }}
+                        
+                        onClick={handleClick}
+                        onTouchCancel={stopClick}
                         className='button'>
-                            <BsMic/>
+                            <BsMic className='mic'/>
                         </button> 
                     </div>
                 </div>
@@ -29,28 +45,33 @@ const LandingPage = () => {
                     <h3 style={{
                         textAlign:'center',
                         fontStyle: 'normal',
-                        fontWeight: 400,
-                        fontSize: 20,
+                        fontWeight:500,
+                        fontSize: 25,
                         lineHeight:3
                         }}>About Us</h3>
 
                     <ReactPlayer
-                    // style={{paddingBottom:20}}
                     width='90%'
+                    height={500}
                     className='video'
-                    url='https://www.youtube.com/watch?v=LXb3EKWsInQ'/>
-                    {/* <iframe src='https://www.youtube.com/watch?v=LXb3EKWsInQ'></iframe> */}
-                    <p>What is this data gathering app?
+                    url='https://youtu.be/XXxOfUTD7Bw'/>
+
+                    
+                    <p className='aboutText'>
+                    <p style={{fontSize:20,fontWeight:500}}>FAQs</p>
+                        <span style={{fontWeight:600}}>What is data gathering application?</span>
                         <br />
                         The Data gathering app is a mobile application from which we are trying to collect nepali voices.
                         <br />
-                        What can we do with this collected Nepali Voice?
+                        <span style={{fontWeight:600}}>What can we do with this collected Nepali Voice?</span>
                         <br />
                         This Dataset of nepali voice helps us in many future AI projects.
                         Many big companies do not provide their datasets to the public so through our application, we are trying to build an open source voice dataset for Nepali languages.
                         Now you can donate your Voice to help us create an open source voice database and be a part of upcoming AI projects.
                         <br />
-                        To use this app you just need to speak the Nepali Sentences showing on your screen by clicking on the mic button and donating your voice.
+                        <span style={{fontWeight:600}}>How to use this application?</span>
+                        <br/>
+                        To use this application you just need to speak the Nepali Sentences shown on your screen by clicking on the mic button and when the backgroundcolor of the mic turns green.Also unclicking the mic button stops the recording. In this way, you can contribute your voice.
                     </p>
                 </div>
             </div>
